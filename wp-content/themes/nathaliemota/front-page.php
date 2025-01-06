23488b4073717620672bb8e6d0e912844e9587d8
Eliezer PEREZ <eliezersierraperez1997@gmail.com>
2025-01-06 03:51:41 +0100

    Commit last config css

--- wp-content/themes/nathaliemota/front-page.php
+++ wp-content/themes/nathaliemota/front-page.php
@@ -60,6 +60,10 @@ get_header();
             <button class="nav-arrow left-arrow">&larr;</button>
             <img class="lightbox-image" src="" alt="Imagem em destaque">
             <button class="nav-arrow right-arrow">&rarr;</button>
+            <div class="single__caption">
+                <span class="single__overlay-title"><?php the_title(); ?></span>
+                <span class="single__overlay-categorie"><?php echo strip_tags(get_the_term_list(get_the_ID(), 'categorie')); ?></span>
+            </div>
         </div>
         <p class="lightbox-caption"></p> <!-- Legenda agora ficará abaixo -->
     </div>
